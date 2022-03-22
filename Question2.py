5342d73477e07097923d687b811e5bbfc748beed
jacelyntayxn <94168656+jacelyntayxn@users.noreply.github.com>
2022-03-22 15:05:05 +0800

    Add files via upload

---
+++ Question2.py
@@ -0,0 +1,5 @@
+#User Input
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+average = (x + y) / 2
+print("The average is " + str(average))
---
+++ Question3b.py
@@ -0,0 +1,4 @@
+#Print odd numbers
+for i in range(102, 66, -1):
+    if (i %2 ==1):
+        print(i)
