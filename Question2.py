daa4a6283766fc65fba859e9ffc684f7e94124d2
jacelyntayxn <94168656+jacelyntayxn@users.noreply.github.com>
2022-03-28 15:26:29 +0800

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,5 +1,7 @@
-#User Input
+#User Input for x and y
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
+#Calculate the average 
 average = (x + y) / 2
+#Print the result 
 print("The average is " + str(average))
